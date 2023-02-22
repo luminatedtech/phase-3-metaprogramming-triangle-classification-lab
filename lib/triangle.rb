ee3188fc2ed4aee993405974a34756aadb940970
Allen Kim <avalmercafe02@gmail.com>
2023-02-22 01:05:58 -0500

    commit tree afbcbb420194bac4c2b9e52c0180e7f1ad103b9e

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,3 +1,28 @@
 class Triangle
-  # write code here
+  class TriangleError < StandardError
+  end 
+  attr_accessor :side1, :side2, :side3 
+  def initialize(side1,side2,side3)
+    @side1 = side1
+    @side2 = side2
+    @side3 = side3 
+  end 
+  def kind 
+    if (side1 = 0 or side2 = 0 or side3 = 0)
+    begin
+      raise TriangleError
+    end
+    elsif (side1 === side2 && side2 === side3 && side1 === side3) 
+      :equilateral
+    elsif (side1 === side2 or side2 === side3 or side1 === side3)
+      :isosceles
+    elsif (side1 != side2 && side1 != side2 && side1 != side3)
+      :scalene
+    elsif (side1 + side2 <= side3 or side1 + side3 <= side2 or side2 + side3 <= side1)
+      begin
+        raise TriangleError
+      end
+      
+    end 
+   end 
 end
